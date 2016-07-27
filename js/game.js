ba053ecd2ecf2b426fe545974a6db4c853194467
Greg Sharpe <gregsharpe3@gmail.com>
2016-07-27 19:33:15 +0100

    Adding control

--- js/game.js
+++ js/game.js
@@ -7,39 +7,57 @@
 // Setup of the game
 
 // Create the canvas from the given element in the HTML
-var canvas = document.getElementById("main");
+var canvas = document.getElementById("main"),
 // Give the canvas 2D context
-var context = canvas.getContext("2d");
+    context = canvas.getContext("2d"),
 
 // Set the canvas with boundaries
 // to allow the user not to go outside
 // the boundaries
-canvas.height = 360;
-canvas.width = 600;
+    height = 360,
+    width = 600,
 
-var keys = [];
+
 
 // Create the player giving some config
-var player {
+    player = {
   // this is where the player will start
-  x : 50,
-  y : height - 30,
-  height : 30,
-  width : 30
-}
+      x : 50,
+      y : height - 30,
+      height : 30,
+      width : 30
+    },
+    keys = [];
+
+canvas.height = height;
+canvas.width = width;
 
 // This function will move the player.
 
 function update() {
 
-  if ( keys[87] || keys[38])
-  {
-    player.y = player.y - 5;
+  if (keys[87]) {
+
+    player.y = player.y - 10;
+
   }
 
-  ctx.clearRect(0,0,width,height);
-  ctx.fillStyle = "red";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+  if (keys[83]){
+    player.y = player.y + 10;
+  }
+
+  if (keys[65]) {
+    player.x = player.x - 10;
+  }
+
+  if (keys[68]) {
+    player.x = player.x + 10;
+  }
+
+
+  context.clearRect(0,0,width,height);
+  context.fillStyle = "red";
+  context.fillRect(player.x, player.y, player.width, player.height);
 
   requestAnimationFrame(update);
 
