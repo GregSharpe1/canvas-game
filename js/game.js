cbff00215eedd6501ef9aa8aeea94ad7b6a45bfa
Greg Sharpe <gregsharpe3@gmail.com>
2016-07-31 21:37:02 +0100

    Added images and invert running

--- js/game.js
+++ js/game.js
@@ -7,17 +7,13 @@
 // Generate a number between 105 and 560.
 // why? in the documentation.
 
-
-
 // Math.floor(Math.random() * (max value - min value)) + min value;
 var randomXPos = Math.floor(Math.random() * (560 - 105)) + 105;
 var randomSizeY = Math.floor(Math.random() * (60 - 20)) + 20;
 var randomYPos = Math.floor(Math.random() * (560 - 105)) + 105;
 var lives = 5;
+
 // Setup of the game
-
-
-
 
 // Create the canvas from the given element in the HTML
 var canvas = document.getElementById("main"),
@@ -46,40 +42,59 @@ var canvas = document.getElementById("main"),
     },
 
     enemy = {
+      // this is where the 'enemy' (obstacles) will be placed.
       x : randomXPos,
       y : height - randomSizeY,
       height : 45,
       width : 30
     },
 
-
+    // Array to hold the keys
     keys = [],
+    // friction of the player
     friction = 0.1,
+    // gravity of the player
     gravity = 0.1;
 
 canvas.width = width;
 canvas.height = height;
 
+// loding the images to canvas
 
-  var kang = new Image();
-  kang.src = "img/kangaroo.jpg";
+// player jumping image
+  var kang_jump = new Image();
+  kang_jump.src = "img/kangaroo-jump.png";
+// player jumping image inverted
+  var kang_jump_inverted = new Image();
+  kang_jump_inverted.src = "img/kangaroo-jump-inverted.png";
+// player running images
+  var kang_running = new Image();
+  kang_running.src = "img/kangaroo-running.gif";
 
+// player running images inverted
+  var kang_running_inverted = new Image();
+  kang_running_inverted.src = "img/kang-running-inverted.gif";
+
+// background images
   var bg = new Image();
   bg.src = "img/background.png";
 
 
   // Add Image for Obstacle.
+  //tumble weed
   var obstacle1 = new Image();
-  obstacle1.src = "img/obstacle1.jpg";
-
+  obstacle1.src = "img/tumbleweed.png";
+  // another obstacle.. haven't thought what yet
   var obstacle2 = new Image();
   obstacle2.src = "img/obstacle2.jpg";
 
 
 
-
+// this function will process keypresses
 function processUserInput() {
 
+  var invert = false;
+  kang_jump.src = "img/kangaroo-jump.png";
   // W  || spacebar (Jump)
   if (keys[87] || keys[32]) {
     if(!player.jumping){
@@ -88,17 +103,21 @@ function processUserInput() {
     }
   }
   // S DOWN
-  if (keys[83]){
+  if (keys[83]){s
     player.y = player.y + 3;
   }
   // A LEFT
   if (keys[65]) {
+    invert = true;
     player.x = player.x - 3;
   }
   // D RIGHT
   if (keys[68]) {
+    invert = false;
     player.x = player.x + 3;
   }
+
+
   player.velX *= friction;
   player.velY += gravity;
   player.x += player.velX;
@@ -117,93 +136,110 @@ function processUserInput() {
       player.jumping = false;
     }
 
+    if (invert == true){
+      kang_jump.src = "img/kangaroo-jump-inverted.png";
+    }
+
 
   // Clear the context
   context.clearRect(0,0,width,height);
   // drawing the character
-  context.drawImage(kang, player.x, player.y);
+  context.drawImage(kang_jump, player.x, player.y);
   // Drawing the enemy
-  context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+  context.drawImage(obstacle1, enemy.x, enemy.y, 40,40);
 
-
+  // if statement to check if it's making contact with the obstacles
   if (
 		player.x <= (enemy.x + 32)
 		&& enemy.x <= (player.x + 32)
 		&& player.y <= (enemy.y + 32)
 		&& enemy.y <= (player.y + 32)
 	){
-    lives--;
-
-
+    // if the lives fall under 1 (aka zero lives)
+    // run game over function
     if (lives < 1){
+
       game_over();
       // alert("Game Over");
 
-    } else {
+    }
+    // if hit an obstacle then run reset function
+    else {
+
     reset();
     }
 
   }
+  // place the score on the canvas
   score();
+  // request frame
   requestAnimationFrame(processUserInput);
 
 }
 
+// this function will place the remaining lives on the canvas
+// and the score
 var score = function() {
 
   var cScore = document.getElementById("score"),
   // Give the canvas 2D context
       ctxScore = canvas.getContext("2d");
 
+      // add the remaining lives to the canvas
   ctxScore.font = "20px Arial";
   ctxScore.fillText("Lives left: " + lives, 10, 20);
 
 
 }
 
-
+// this function will run when lives are below 0
+// it will add a game over layer to the canvas and
+// reset the game
 
 var game_over = function() {
 
-  lives == 5;
+
   var cGame_Over = document.getElementById("game_over"),
   // Give the canvas 2D context
       cxtGame_Over = canvas.getContext("2d");
 
+      // add text to the canvas saying "game over"
   cxtGame_Over.font = "50px Arial";
   cxtGame_Over.fillText("Game Over", 155, 120);
-  reset();
-
 
 
 }
 
-
+// lsiten for key presses down
 document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
 });
-
+// listen for when the keypress has been released
 document.body.addEventListener("keyup", function(e) {
     keys[e.keyCode] = false;
 });
-
+// listen for the key presses on these functions
 window.addEventListener("load",function(){
     processUserInput();
-    drawObstacles();
 });
 
+// do i need this function?
+// this function will end the game and display the score
+var end_game = function() {
+
+}
+
+// this function will reset the player to the start position
 var reset = function() {
 
-
+  // decrease the lives by one every time the game is reset
+  lives--;
   // Kind of works
 
   // Sets the player to starting point
   player.x = 30;
   player.y = height - 30;
 
-
   // TODO: Randomly move the enemy.
-
-
 
 }
